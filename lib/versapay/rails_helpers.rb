a95b01671a2e89cac0a2a4ee6c2aef590dd5f115
Sean Walberg <sean@ertw.com>
2011-10-06 15:04:39 -0500

    Added payment checkout links

--- lib/versapay/rails_helpers.rb
+++ lib/versapay/rails_helpers.rb
@@ -8,5 +8,13 @@ module Versapay
       end
       "<a href=\"#{link}\">#{anchor}</a>"
     end
+
+    def payment_checkout_link_to(anchor, message = "Credit Card payment", opts = {})
+      link = "https://" + Versapay::site + "/send_money?api_token=#{Versapay.token}&message=#{html_escape(message).gsub(/ /, "+")}"
+      opts.each do |k, v|
+        link += "&#{k}=#{html_escape(v)}"
+      end
+      "<a href=\"#{link}\">#{anchor}</a>"
+    end
   end
 end
--- test/helper_test.rb
+++ test/helper_test.rb
@@ -15,4 +15,9 @@ class HelperTest < ActionView::TestCase
     assert_equal expected, debit_agreement_link_to("test", "Testing 123", {:reference => "abc123"})
   end
 
+  test "creating a credit card link" do
+    expected = '<a href="https://demo.versapay.com/send_money?api_token=' + Versapay::token + '&message=Testing+123&pref=cc">test</a>'
+    assert_equal expected, payment_checkout_link_to("test", "Testing 123", {:pref => "cc"})
+  end
+
 end
