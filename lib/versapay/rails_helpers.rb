03f3101d3b7cd054381d160d6a87ae56afbb66aa
James Herdman <james.herdman@me.com>
2013-03-22 10:23:04 -0400

    Refactor Rails link helpers
    All helpers use Versapay.uri underneath the hood. This also fixed a
    problem wherein arbitrary parameters were not being appended to links.

--- lib/versapay/rails_helpers.rb
+++ lib/versapay/rails_helpers.rb
@@ -1,32 +1,42 @@
+require 'active_support/core_ext/hash/reverse_merge'
+
 module Versapay
   module Helpers
     # Provides a debit agreement link
-    def debit_agreement_link_to(anchor, message = "Debit agreement", opts = {})
-      link = debit_agreement_link(message, opts)
-      "<a href=\"#{link}\">#{anchor}</a>"
+    def debit_agreement_link_to(inner_text, message = "Debit agreement", opts = {})
+      build_anchor_tag(inner_text, debit_agreement_link(message, opts))
     end
 
-    def payment_checkout_link_to(anchor, message = "Credit Card payment", opts = {})
-      link = payment_checkout_link(message, opts)
-      "<a href=\"#{link}\">#{anchor}</a>"
+    def payment_checkout_link_to(inner_text, message = "Credit Card payment", opts = {})
+      build_anchor_tag(inner_text, payment_checkout_link(message, opts))
     end
 
     def debit_agreement_link(message = "Debit agreement", opts = {})
-      link = "https://" + Versapay::site + "/authorize?api_token=#{Versapay.token}&message=#{html_escape(message).gsub(/ /, "+")}"
-      opts.each do |k, v|
-        link += "&#{k}=#{html_escape(v)}"
-      end
+      default_options = {
+        :message => message,
+        :api_token => Versapay.token
+      }
 
-      link
+      options = default_options.reverse_merge(opts)
+
+      Versapay.uri('/authorize', options)
     end
 
     def payment_checkout_link(message = "Credit Card payment", opts = {})
-      link = "https://" + Versapay::site + "/send_money?api_token=#{Versapay.token}&message=#{html_escape(message).gsub(/ /, "+")}"
-      opts.each do |k, v|
-        link += "&#{k}=#{html_escape(v)}"
-      end
+      default_options = {
+        :message => message,
+        :api_token => Versapay.token
+      }
 
-      link
+      options = default_options.reverse_merge(opts)
+
+      Versapay.uri('/send_money', options)
+    end
+
+    private
+
+    def build_anchor_tag(inner_text, uri)
+      %{<a href="#{uri}">#{inner_text}</a>}
     end
   end
 end
